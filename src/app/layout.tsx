7bec2752ea54245f41d5ac4a05c1a890f63e6cd2
rfks <15876610+rfks@users.noreply.github.com>
2024-09-15 20:26:54 +0200

    add cleck auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ConvexClientProvider } from "./convex-client-provider";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,9 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ConvexClientProvider>
+        <Providers>
           {children}
-        </ConvexClientProvider>
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,105 +1,32 @@
 'use client';
 
-// import { useState } from "react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { NewToDoForm } from "./_components/new-todo-form";
-import { useQuery, useMutation } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { Id } from "../../convex/_generated/dataModel";
-
-// type ToDoItem={
-//   title:string;
-//   description:string;
-//   completed:boolean;
-// }
+import { ToDoList } from "./to-do-list";
+import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
 
 export default function Home() {
-  // const [todos,setTodos]=useState<ToDoItem[]>([
-  //   {title:"Example",description:"This is an example",completed:false}
-  // ]);
-
-  const todos=useQuery(api.functions.listTodos);
 
   return (
     <div className="max-w-screen-md mx-auto p-4 space-y-4">
-      <h1 className="text-xl font-bold">To-do List</h1>
-      <ul className="space-y-2">
-      {todos?.map(({_id,title,description,completed},index)=>(
-        <ToDoItem 
-          key={index}
-          id={_id}
-          title={title} 
-          description={description} 
-          completed={completed} 
-          // onCompleteChanged={(newValue) =>{
-          //   setTodos(prev=>{
-          //     const newTodos=[...prev];
-          //     newTodos[index].completed=newValue;
-          //     return newTodos;
-          //   })
-          // }}
-          // onRemove={() => {
-          //   setTodos(prev => {
-          //     const newTodos = [...prev].filter((_,i)=> i !== index);
-          //     return newTodos;
-          //   })
-          // }}
-          />
-      ))}
-      </ul>
-      <NewToDoForm 
-      // onCreate={(title,description) => {
-      //   setTodos(prev =>{
-      //     const newTodos = [...prev];
-      //     newTodos.push({title,description,completed:false});
-      //     return newTodos;
-      //   });
-      // }}
-      />
-
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold">To-do List</h1>
+        <UserButton />
+      </div>
+      <Authenticated>
+        <ToDoList />
+        <NewToDoForm />
+      </Authenticated>
+      <Unauthenticated>
+        <p className="text-gray-600">Please sign in to continue</p>
+        <SignInButton>
+          <button className="p-1 bg-blue-500 text-white rounded">Sign In</button>
+        </SignInButton>
+      </Unauthenticated>
+      <AuthLoading>
+        Loading..
+      </AuthLoading>
     </div>
   );
 }
 
-function ToDoItem(
-  {
-    id,
-    title, 
-    description, 
-    completed, 
-    // onCompleteChanged, 
-    // onRemove
-  } :{
-    id: Id<"todos">,
-    title: string,
-    description: string,
-    completed: boolean,
-  // onCompleteChanged: (newValue: boolean) => void,
-  // onRemove: () => void,
-}) {
-  const updateTodo = useMutation(api.functions.updateTodo);
-  const deleteTodo = useMutation(api.functions.deleteTodo);
-  return(
-    <li className="w-full items-center flex gap-2 border rounded p-2">
-      <input 
-        type="checkbox" 
-        checked={completed} 
-        onChange={e => 
-          updateTodo({id, completed:e.target.checked})
-          // onCompleteChanged(e.target.checked)
-          }/>
-      <div>
-        <p className="font-semibold">{title}</p>
-        <p className="text-sm text-gray-600">{description}</p>
-      </div>
-      <div className="ml-auto">
-        <button 
-          type="button" 
-          className="text-red-500" 
-          onClick={() => deleteTodo({id})
-            // ()=>onRemove()
-          }>Remove
-        </button>
-      </div>
-    </li>
-  );
-}
